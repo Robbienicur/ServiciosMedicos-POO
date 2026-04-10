2e27f804d3bed2ed0ca5fad0eb9b81e90e569bd5
Robbienicur <robbienicur@gmail.com>
2026-04-10 17:53:08 -0600

    Actualizar paleta de colores UDLAP

--- src/Utilidades/ColoresUDLAP.java
+++ src/Utilidades/ColoresUDLAP.java
@@ -2,22 +2,44 @@ package Utilidades;
 
 import java.awt.Color;
 
-public class ColoresUDLAP {
-    public static final Color VERDE = new Color(0, 102, 0, 100);
-    public static final Color VERDE_HOVER = new Color(0, 102, 0, 170);
-    public static final Color NARANJA = new Color(255, 102, 0, 100);
-    public static final Color NARANJA_HOVER = new Color(255, 102, 0, 170);
-    public static final Color ROJO = new Color(221, 71, 66, 100);
-    public static final Color ROJO_HOVER = new Color(221, 71, 66, 220);
-    public static final Color NARANJA_BARRA = new Color(255, 102, 0);
-    public static final Color VERDE_OSCURO = new Color(0, 100, 0);
+public final class ColoresUDLAP {
+    public static final Color VERDE_PRIMARIO = new Color(0, 102, 0);
+    public static final Color VERDE_HOVER = new Color(0, 82, 0);
+    public static final Color VERDE_SUAVE = new Color(232, 245, 233);
+
+    public static final Color NARANJA_ACENTO = new Color(255, 102, 0);
+    public static final Color NARANJA_HOVER = new Color(229, 90, 0);
+    public static final Color NARANJA_SUAVE = new Color(255, 243, 230);
+
+    public static final Color FONDO_GENERAL = new Color(248, 249, 250);
+    public static final Color FONDO_CARD = Color.WHITE;
+    public static final Color FONDO_NEUTRO = new Color(241, 243, 245);
+
+    public static final Color BORDE = new Color(222, 226, 230);
+    public static final Color BORDE_HOVER = new Color(206, 212, 218);
+
+    public static final Color TEXTO_PRINCIPAL = new Color(45, 52, 54);
+    public static final Color TEXTO_SECUNDARIO = new Color(99, 110, 114);
+
+    public static final Color ROJO_ERROR = new Color(214, 48, 49);
+
+    // Legacy aliases
+    public static final Color VERDE = VERDE_PRIMARIO;
+    public static final Color VERDE_SOLIDO = VERDE_PRIMARIO;
+    public static final Color VERDE_OSCURO = VERDE_PRIMARIO;
+    public static final Color VERDE_HOVER_LEGACY = new Color(0, 102, 0, 170);
+    public static final Color NARANJA = NARANJA_ACENTO;
+    public static final Color NARANJA_HOVER_LEGACY = new Color(255, 102, 0, 170);
+    public static final Color NARANJA_BARRA = NARANJA_ACENTO;
+    public static final Color ROJO = ROJO_ERROR;
+    public static final Color ROJO_HOVER = new Color(214, 48, 49, 220);
+    public static final Color ROJO_SOLIDO = ROJO_ERROR;
     public static final Color BLANCO = Color.WHITE;
-    public static final Color GRIS_CLARO = new Color(211, 211, 211); // Color gris claro
-    public static final Color NEGRO = new Color(0, 0, 0); // Color negro
-    public static final Color VERDE_SOLIDO = new Color(0, 102, 0);
-    public static final Color ROJO_SOLIDO = new Color(221, 71, 66);
+    public static final Color NEGRO = TEXTO_PRINCIPAL;
+    public static final Color GRIS_CLARO = BORDE;
+    public static final Color GRIS_OSCURO = TEXTO_SECUNDARIO;
     public static final Color AMARILLO = Color.YELLOW;
     public static final Color AZUL = Color.BLUE;
-    public static final Color GRIS_OSCURO = new Color(102, 102, 102);
 
+    private ColoresUDLAP() {}
 }
